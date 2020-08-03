aff0be5bb93b6a8b102c9ca5d1e5d82402ee72e7
Tobey <bitsqiu@gmail.com>
2020-08-03 16:48:18 +0800

    [di.qin] fixed: finish last frame one spare or one strike.

--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -25,10 +25,26 @@ public class BowlingGameTest {
     @Test
     void no_trike_and_no_spare_until_last_frame() {
         bowlingGame.frame(1, 0).frame(1, 0).frame(1, 0).frame(1, 0).frame(1, 0).frame(1, 0).frame(1, 0).frame(1, 0).frame(1, 0);
-        bowlingGame.frame(1,1);
-        bowlingGame.frame(1,1);
-        assertEquals(13,bowlingGame.getScore());
+        bowlingGame.frame(1, 1);
+        assertEquals(11, bowlingGame.getScore());
     }
 
+    @Test
+    void one_spare_and_no_strike_until_last_frame() {
+        bowlingGame.frame(1, 0).frame(1, 0).frame(1, 0).frame(1, 0).frame(1, 0).frame(1, 0).frame(1, 0).frame(1, 0).frame(1, 0);
+        bowlingGame.frame(2, 8);
+        bowlingGame.frame(1, 0);
+        assertEquals(20, bowlingGame.getScore());
+    }
+
+    @Test
+    void one_strike_and_no_spare_until_last_frame(){
+        bowlingGame.frame(1, 0).frame(1, 0).frame(1, 0).frame(1, 0).frame(1, 0).frame(1, 0).frame(1, 0).frame(1, 0).frame(1, 0);
+        bowlingGame.frame(10,0);
+        bowlingGame.frame(1,3);
+        assertEquals(23,bowlingGame.getScore());
+    }
+
+    @Test
 
 }
