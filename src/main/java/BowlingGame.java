24dbccb8c0e4ec99e2883c7dd0fe4c4841a459d9
Tobey <bitsqiu@gmail.com>
2020-08-03 16:31:07 +0800

    [di.qin] fixed: finish one frame and one throw

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -3,6 +3,12 @@ import java.util.Stack;
 public class BowlingGame {
     private Stack<Frame> frames = new Stack<>();
 
+    public BowlingGame frame(int firstThrow, int secondThrow) {
+        Frame frame = new Frame(firstThrow, secondThrow);
+        frames.push(frame);
+        return this;
+    }
+
     public int getScore() {
         return frames.stream().mapToInt(Frame::score).sum();
     }
--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -3,10 +3,22 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BowlingGameTest {
+    BowlingGame bowlingGame = new BowlingGame();
 
     @Test
     void no_throw() {
-        BowlingGame bowlingGame = new BowlingGame();
         assertEquals(0, bowlingGame.getScore());
+    }
+
+    @Test
+    void one_frame_and_one_throw() {
+        bowlingGame.frame(8, 0);
+        assertEquals(8, bowlingGame.getScore());
+    }
+
+    @Test
+    void three_frame_and_on_strike_and_no_spare() {
+        bowlingGame.frame(1, 0).frame(3, 3);
+        assertEquals(7,bowlingGame.getScore());
     }
 }
